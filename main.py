99d06b4d87b6593773c291a9c3a50aa229dfa9ac
sherzod <azamovshavkat98@gmail.com>
2024-06-04 16:18:59 +0500

    The project is ready to upload to the server

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from datum import use_playwright, get_calls_fast
 
 app = FastAPI()
 router = APIRouter()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @router.get('/data')
